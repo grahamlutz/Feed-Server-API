6c2e8b38e82cbecdd7728504e2fc71806c47423f
Graham Lutz <graham.lutz@gmail.com>
2016-07-26 22:30:48 -0400

    fix null issues

--- Controllers/Extensions.cs
+++ Controllers/Extensions.cs
@@ -4,6 +4,7 @@ public static class Extensions
 {
     public static bool ContainsCaseInsensitive(this string s, string txt)
     {
+    	if(s == null) return false;
         return s.IndexOf(txt, StringComparison.OrdinalIgnoreCase) >= 0;
     }
 }
